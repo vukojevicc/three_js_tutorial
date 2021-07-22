6a6f7dcc85c6075222ec36613dfddf92f15a8b78
Aleksandar <aleksandar.vukojevicc@gmail.com>
2021-07-22 13:59:56 +0200

    npm live server installed

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const scrollable = document.querySelector('.scrollable');
+
+let current = 0;
+let target = 0;
+let ease = 0.075;
+
+// Linear interpolation used for smooth scrolling and image offset uniformt adjustment
+function lerp(start, end, time){
+    return ((end - start) * time) + start;
+}
+
+function init(){
+    document.body.style.height = `${scrollable.getBoundingClientRect().height}px`;
+}
+
+function smoothScroll(){
+    target = window.scrollY;
+    current = lerp(current, target, ease);
+}
